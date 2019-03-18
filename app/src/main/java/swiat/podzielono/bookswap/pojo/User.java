09dffaa87e70d949f4fb5e2b8aec2a0830b426bf
wadziuxxx <macwadowski@tlen.pl>
2019-03-18 13:10:09 +0100

    User class

--- app/src/main/java/swiat/podzielono/bookswap/pojo/User.java
+++ app/src/main/java/swiat/podzielono/bookswap/pojo/User.java
@@ -1,4 +1,20 @@
 package swiat.podzielono.bookswap.pojo;
 
 public class User {
+
+    private String email;
+    private String name;
+
+    public User(String email, String name) {
+        this.email = email;
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
