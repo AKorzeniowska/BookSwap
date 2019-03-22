4359e766155fa22280efdca5780a623be59a4116
Agata <agata.korzeniowska@o2.pl>
2019-03-23 00:55:32 +0100

    Enabled using menu buttons

--- app/src/main/java/swiat/podzielono/bookswap/BrowseActivity.java
+++ app/src/main/java/swiat/podzielono/bookswap/BrowseActivity.java
@@ -5,6 +5,9 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -72,13 +75,22 @@ public class BrowseActivity extends AppCompatActivity {
         });
     }
 
-    public void addBookActivityStarter (View view){
+    public void addBookActivityStarter (MenuItem item){
         Intent intent = new Intent(this, AddBookActivity.class);
         startActivity(intent);
     }
 
-    public void openChatActivity(View view) {
+    public void openChatActivity(MenuItem item) {
         Intent intent = new Intent(this, ChatActivity.class);
         startActivity(intent);
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.navigation, menu);
+        return true;
+    }
+
 }
