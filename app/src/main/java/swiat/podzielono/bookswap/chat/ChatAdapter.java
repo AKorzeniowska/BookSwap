f6be56a63658d0c8192c3b5a0ae2793b6551d415
wadziuxxx <macwadowski@tlen.pl>
2019-03-22 22:25:21 +0100

    chat adapter to fix

---
+++ app/src/main/java/swiat/podzielono/bookswap/chat/ChatAdapter.java
@@ -0,0 +1,82 @@
+package swiat.podzielono.bookswap.chat;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.*;
+import swiat.podzielono.bookswap.R;
+import swiat.podzielono.bookswap.data.BookObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder> {
+
+    private Context context;
+    private DatabaseReference mDatabaseReference;
+    private FirebaseUser mFirebaseUser;
+    private List<String> mItemList;
+
+    private String[] dummyData = {
+            "smth",":))", "12", "123", "123123", "12312312"
+    };
+
+    public ChatAdapter(Context context) {
+        this.context = context;
+        mFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference().child("owners");
+
+        mItemList = new ArrayList<>();
+        mDatabaseReference.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull final DataSnapshot dataSnapshot) {
+                Iterable <DataSnapshot> users = dataSnapshot.getChildren();
+                for (DataSnapshot snap: users) {
+                    String x = snap.getKey();
+                    mItemList.add(x);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @NonNull
+    @Override
+    public ChatViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view =  LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.chat_element, viewGroup, false);
+        ChatViewHolder myViewHolder = new ChatViewHolder(view);
+        return myViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ChatViewHolder chatViewHolder, int i) {
+        chatViewHolder.mUserNameTextView.setText(mItemList.get(i));
+    }
+
+    @Override
+    public int getItemCount() {
+        return  mItemList.size();
+    }
+
+    public class ChatViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView mUserNameTextView;
+
+        public ChatViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mUserNameTextView = itemView.findViewById(R.id.chat_list_element);
+        }
+    }
+}
