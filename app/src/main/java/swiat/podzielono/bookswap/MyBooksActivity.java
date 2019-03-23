3207540975e964fb8b41f55afaafab0b405aa2af
Agata <agata.korzeniowska@o2.pl>
2019-03-23 13:00:50 +0100

    Started working on SearchActivity

--- app/src/main/java/swiat/podzielono/bookswap/MyBooksActivity.java
+++ app/src/main/java/swiat/podzielono/bookswap/MyBooksActivity.java
@@ -1,8 +1,15 @@
 package swiat.podzielono.bookswap;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -17,22 +24,35 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.List;
 
+import swiat.podzielono.bookswap.data.BookAdapter;
 import swiat.podzielono.bookswap.data.BookObject;
+import swiat.podzielono.bookswap.ui.ProfileActivity;
 
-public class MyBooksActivity extends AppCompatActivity {
+public class MyBooksActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DatabaseReference mDatabaseUsersReference;
     private DatabaseReference mDatabaseBooksReference;
     private ArrayList<BookObject> booksList;
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
+
+    private BookAdapter bookAdapter;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_books);
 
+        mDrawerLayout = findViewById(R.id.drawer_layout);
         mDatabaseUsersReference = FirebaseDatabase.getInstance().getReference().child("owners");
         mDatabaseBooksReference = FirebaseDatabase.getInstance().getReference().child("books");
         booksList = new ArrayList<>();
+
+        listView = findViewById(R.id.book_list);
+        mNavigationView = findViewById(R.id.nav_view);
+
+        View view = mNavigationView.getHeaderView(0);
     }
 
     @Override
@@ -65,6 +85,8 @@ public class MyBooksActivity extends AppCompatActivity {
                         public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                             BookObject currentBook = dataSnapshot.getValue(BookObject.class);
                             booksList.add(currentBook);
+                            bookAdapter = new BookAdapter(MyBooksActivity.this, booksList);
+                            listView.setAdapter(bookAdapter);
                         }
 
                         @Override
@@ -80,5 +102,33 @@ public class MyBooksActivity extends AppCompatActivity {
                 Toast.makeText(MyBooksActivity.this, databaseError.getMessage(), Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.navigation, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+
+        if (id == R.id.nav_sign_out) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            FirebaseAuth.getInstance().signOut();
+            finish();
+        } else if (id == R.id.nav_my_books) {
+            Intent intent = new Intent(this, MyBooksActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_favourite_books) {
+            //in future
+        } else if (id == R.id.nav_profile) {
+            Intent intent = new Intent(this, ProfileActivity.class);
+            startActivity(intent);
+        }
+        return true;
     }
 }
