a5c84f7bce9164c9ede949ae08c4ba0f1f5dd2bc
Agata <agata.korzeniowska@o2.pl>
2019-03-23 03:30:34 +0100

    Expanded features of added book

--- app/src/main/java/swiat/podzielono/bookswap/AddBookActivity.java
+++ app/src/main/java/swiat/podzielono/bookswap/AddBookActivity.java
@@ -10,8 +10,11 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -20,8 +23,11 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
@@ -36,6 +42,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
 
 import swiat.podzielono.bookswap.data.BookObject;
 
@@ -44,8 +51,16 @@ public class AddBookActivity extends AppCompatActivity {
     private EditText mBookAuthor;
     private EditText mBookTitle;
     private EditText mBookPrice;
+    private EditText mDescription;
+    private EditText mYear;
+    private EditText mPublisher;
+    private EditText mCustomCategory;
+
+    private ArrayAdapter<String> adapter;
+
     private DatabaseReference mDatabaseUserReference;
     private DatabaseReference mDatabaseBookReference;
+    private DatabaseReference mDatabaseCategoryReference;
     private String currentUser;
 
     private StorageReference mStorageReference;
@@ -55,8 +70,14 @@ public class AddBookActivity extends AppCompatActivity {
     private ImageView mImageView;
     private ImageView mImageView2;
     private ImageView mImageView3;
+
     List<Uri> imageUri = new ArrayList<>();
     List<Uri> uploadedImageUri = new ArrayList<>();
+
+    private Spinner mMainCategorySpinner;
+    private Spinner mSecondaryCategorySpinner;
+    List<String> dataForMainSpinner = new ArrayList<>();
+    List<String> dataForSecondarySpinner = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +87,10 @@ public class AddBookActivity extends AppCompatActivity {
         mBookAuthor = findViewById(R.id.book_author_text);
         mBookTitle = findViewById(R.id.book_title_text);
         mBookPrice = findViewById(R.id.book_price_text);
+        mDescription = findViewById(R.id.book_description_text);
+        mCustomCategory = findViewById(R.id.book_custom_category_textFill);
+        mYear = findViewById(R.id.book_year_text);
+        mPublisher = findViewById(R.id.book_publisher_text);
 
         mImageView = findViewById(R.id.book_image_first);
         mImageView2 = findViewById(R.id.book_image_second);
@@ -74,8 +99,94 @@ public class AddBookActivity extends AppCompatActivity {
         currentUser = FirebaseAuth.getInstance().getCurrentUser().getDisplayName();
         mDatabaseUserReference = FirebaseDatabase.getInstance().getReference().child("owners").child(currentUser).child("my books");
         mDatabaseBookReference = FirebaseDatabase.getInstance().getReference().child("books");
+        mDatabaseCategoryReference = FirebaseDatabase.getInstance().getReference().child("categories");
 
         mStorageReference = FirebaseStorage.getInstance().getReference();
+
+        mMainCategorySpinner = findViewById(R.id.book_main_category_list);
+        mSecondaryCategorySpinner = findViewById(R.id.book_secondary_category_list);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        spinnerDataSetter();
+        secondarySpinnerDataSetter();
+    }
+
+
+
+    private void spinnerDataSetter() {
+        final boolean[] done = new boolean[1];
+        mDatabaseCategoryReference.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Iterable<DataSnapshot> categories = dataSnapshot.getChildren();
+                for (DataSnapshot category : categories) {
+                    String main = category.getKey();
+                    dataForMainSpinner.add(main);
+
+              }
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                        AddBookActivity.this,
+                        android.R.layout.simple_spinner_item,
+                        dataForMainSpinner
+                );
+
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                mMainCategorySpinner.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(AddBookActivity.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+    private void secondarySpinnerDataSetter(){
+        mMainCategorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                String x = mMainCategorySpinner.getSelectedItem().toString();
+                mDatabaseCategoryReference.child(x).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        dataForSecondarySpinner.clear();
+                        Iterable<DataSnapshot> secondaries = dataSnapshot.getChildren();
+                        for (DataSnapshot secondary : secondaries){
+                            String sec = secondary.getKey();
+                            dataForSecondarySpinner.add(sec);
+
+                            ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                                    AddBookActivity.this,
+                                    android.R.layout.simple_spinner_item,
+                                    dataForSecondarySpinner
+                            );
+                            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                            mSecondaryCategorySpinner.setAdapter(adapter);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
 
     private void addBookToDatabase() {
@@ -83,6 +194,12 @@ public class AddBookActivity extends AppCompatActivity {
         String bookTitle = mBookTitle.getText().toString().trim();
         String bookPrice = mBookPrice.getText().toString().trim();
         String currentDate = new SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH).format(new Date());
+        String year = mYear.getText().toString().trim();
+        String mainCategory = mMainCategorySpinner.getSelectedItem().toString();
+        String secondCategory = mSecondaryCategorySpinner.getSelectedItem().toString();
+        String description = mDescription.getText().toString().trim();
+        String publisher = mPublisher.getText().toString().trim();
+        String customCategory = mCustomCategory.getText().toString().trim();
 
         final String hashcode = mDatabaseBookReference.push().getKey();
 
@@ -92,7 +209,7 @@ public class AddBookActivity extends AppCompatActivity {
                 uri[i] = uploadedImageUri.get(i).toString();
             }
         }
-        BookObject bookToAdd = new BookObject(bookTitle, bookAuthor, null, null, null, null, null, currentUser, null, bookPrice, uploadedImageUri.get(0).toString(), null, null, null, currentDate);
+        BookObject bookToAdd = new BookObject(bookTitle, bookAuthor, year, publisher, mainCategory, secondCategory, customCategory, currentUser, null, bookPrice, uploadedImageUri.get(0).toString(), null, null, description, currentDate);
 
         mDatabaseBookReference.child(hashcode).setValue(bookToAdd).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
@@ -177,7 +294,7 @@ public class AddBookActivity extends AppCompatActivity {
                             @Override
                             public void onSuccess(Uri uri) {
                                 uploadedImageUri.add(uri);
-                                    addBookToDatabase();
+                                addBookToDatabase();
                             }
                         });
                         Toast.makeText(AddBookActivity.this, "Uploaded", Toast.LENGTH_SHORT).show();
