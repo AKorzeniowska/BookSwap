e3dd8b81bfd0732462bbca00b5a63fdeb0848652
wadziuxxx <macwadowski@tlen.pl>
2019-04-06 11:21:40 +0200

    Login bug fixed

--- app/src/main/java/swiat/podzielono/bookswap/MainActivity.java
+++ app/src/main/java/swiat/podzielono/bookswap/MainActivity.java
@@ -57,10 +57,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
+        if (mAuth == null) {
+            mAuth = FirebaseAuth.getInstance();
+        }
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if (currentUser != null) {
             startBrowseActivity();
         }
+
     }
 
     public void signIn(View view) {
